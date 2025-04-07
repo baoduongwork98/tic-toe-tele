959cf5f9fd707aa3be0abca958ccf3bec8b00b43
Baoduongg <baoduongwork98@gmail.com>
2025-04-07 22:28:07 +0700

    update

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+      <TicTacToe />
+    </div>
+  </template>
+  
+  <script setup>
+  import TicTacToe from './components/TicTacToe.vue'
+  </script>
+  
+  <style>
+  #app {
+    text-align: center;
+    margin-top: 50px;
+  }
+  </style>
+  
